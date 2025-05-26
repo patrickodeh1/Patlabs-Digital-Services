bead2951ad8b718a2d63d1ead3e39c86518a92b6
Patrick Odeh <vyltyan@gmail.com>
2025-05-26 12:46:47 +0100

    preparing to deploy

--- soft_tech/settings.py
+++ soft_tech/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mz=87j)o6nrz6*1vgzf02!-u+#s5#)bg87q6*rh_%-0m#%))a9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['soft-tech.onrender.com']
+
+
 
 
 # Application definition
@@ -116,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
